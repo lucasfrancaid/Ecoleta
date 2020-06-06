b1163fe17ad4a08f7ea4c73a4e087e205533552d
lucasfrancaid <lucasfrancaid@gmail.com>
2020-06-06 13:00:47 -0300

    The home page and the page for registering Points were created.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,32 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import api from './services/api';
+
+import Routes from './routes';
 
 function App() {
-  const [message, setMessage] = useState({English:'Hello', Portuguese:'Ola'});
-
-  useEffect(() => {
-    async function Welcome() {
-      const response = await api.get('/welcome')
-      return setMessage({English: response.data.English, Portuguese: response.data.Portuguese});
-    };
-    Welcome();
-  });
-  
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          {message.English}
-        </p>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {message.Portuguese}
-        </p>
-      </header>
-    </div>
-  );
-}
+    <Routes />
+  ); 
+};
 
 export default App;
